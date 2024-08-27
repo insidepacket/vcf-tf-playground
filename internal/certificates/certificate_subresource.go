033ad36ab620d27afda27e09259a441b2d69cc2d
David Zhang <davidzhang@Davids-Air.modem>
2024-08-28 08:24:19 +1000

    remove FlattenCertificateWithSubject from the subresource.go

--- internal/certificates/certificate_subresource.go
+++ internal/certificates/certificate_subresource.go
@@ -5,10 +5,7 @@
 package certificates
 
 import (
-	"strings"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/vmware/vcf-sdk-go/models"
 )
 
 func CertificateSchema() *schema.Resource {
@@ -179,65 +176,3 @@ func CertificateSchema() *schema.Resource {
 		return result
 	}
 */
-func FlattenCertificateWithSubject(cert *models.Certificate) map[string]interface{} {
-	result := make(map[string]interface{})
-	if cert.Domain == nil {
-		result["domain"] = "nil"
-	} else {
-		result["domain"] = *cert.Domain
-	}
-	if cert.GetCertificateError == nil {
-		result["certificate_error"] = "nil"
-	} else {
-		result["certificate_error"] = *cert.GetCertificateError
-	}
-
-	result["expiration_status"] = *cert.ExpirationStatus
-	result["issued_by"] = *cert.IssuedBy
-	result["issued_to"] = *cert.IssuedTo
-	result["key_size"] = *cert.KeySize
-	result["not_after"] = *cert.NotAfter
-	result["not_before"] = *cert.NotBefore
-	result["number_of_days_to_expire"] = *cert.NumberOfDaysToExpire
-	result["pem_encoded"] = *cert.PemEncoded
-	result["public_key"] = *cert.PublicKey
-	result["public_key_algorithm"] = *cert.PublicKeyAlgorithm
-	result["serial_number"] = *cert.SerialNumber
-	result["signature_algorithm"] = *cert.SignatureAlgorithm
-	result["subject"] = *cert.Subject
-	result["subject_alternative_name"] = cert.SubjectAlternativeName
-	result["thumbprint"] = *cert.Thumbprint
-	result["thumbprint_algorithm"] = *cert.ThumbprintAlgorithm
-	result["version"] = *cert.Version
-
-	// Parse the subject string to extract CN, OU, O, L, ST, C
-	subjectDetails := parseSubject(*cert.Subject)
-
-	// Add parsed subject components to the result map
-	result["subject_cn"] = subjectDetails["CN"]
-	result["subject_ou"] = subjectDetails["OU"]
-	result["subject_org"] = subjectDetails["O"]
-	result["subject_locality"] = subjectDetails["L"]
-	result["subject_st"] = subjectDetails["ST"]
-	result["subject_country"] = subjectDetails["C"]
-
-	return result
-}
-
-func parseSubject(subject string) map[string]string {
-	parsedSubject := make(map[string]string)
-
-	// Split the subject string by commas to separate key-value pairs
-	subjectParts := strings.Split(subject, ",")
-
-	for _, part := range subjectParts {
-		// Split each part by the equals sign to separate the key and value
-		keyValue := strings.SplitN(strings.TrimSpace(part), "=", 2)
-		if len(keyValue) == 2 {
-			// Store the value in the map with the key as the subject component
-			parsedSubject[keyValue[0]] = keyValue[1]
-		}
-	}
-
-	return parsedSubject
-}
